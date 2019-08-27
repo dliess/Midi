4d780f8c816e0dfe9dbec7f6c5d820dcf3ae7ac1
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2019-08-27 22:26:15 +0200

    added dummy media

---
+++ MidiMedium/Dummy/MidiMediumDummy.cpp
@@ -0,0 +1,76 @@
+#include "MidiMediumDummy.h"
+
+using namespace midi;
+
+MidiInMediumDummy::MidiInMediumDummy(const std::string& usbDeviceName, IMidiMedium::Type type) : 
+    m_usbDeviceName(usbDeviceName),
+    m_type(type)
+{
+}
+
+IMidiMedium::Type MidiInMediumDummy::getType() const
+{
+    return m_type;
+}
+
+std::string MidiInMediumDummy::getPortName() const
+{
+    return "";
+}
+
+std::string MidiInMediumDummy::getDeviceName() const
+{
+    return m_usbDeviceName;
+}
+
+void MidiInMediumDummy::registerCallback(Callback cb)
+{
+
+}
+
+void MidiInMediumDummy::update()
+{
+
+}
+
+MidiOutMediumDummy::MidiOutMediumDummy(const std::string& usbDeviceName, IMidiMedium::Type type) : 
+    m_usbDeviceName(usbDeviceName),
+    m_type(type)
+{
+}
+
+IMidiMedium::Type MidiOutMediumDummy::getType() const
+{
+    return m_type;
+}
+
+std::string MidiOutMediumDummy::getPortName() const
+{
+    return "";
+}
+
+std::string MidiOutMediumDummy::getDeviceName() const
+{
+    return m_usbDeviceName;
+}
+
+bool MidiOutMediumDummy::send(const std::vector<uint8_t>& message)
+{
+    return true;
+}
+
+MidiMediumDummy::MidiMediumDummy(const std::string& usbDeviceName, IMidiMedium::Type type) :
+    m_pMidiInMediumDummy(std::make_unique<MidiInMediumDummy>(usbDeviceName, type)),
+    m_pMidiOutMediumDummy(std::make_unique<MidiOutMediumDummy>(usbDeviceName, type))
+{    
+}
+
+std::unique_ptr<MidiInMediumDummy> MidiMediumDummy::hijackInMedium()
+{
+    return std::move(m_pMidiInMediumDummy);
+}
+
+std::unique_ptr<MidiOutMediumDummy> MidiMediumDummy::hijackOutMedium()
+{
+    return std::move(m_pMidiOutMediumDummy);
+}
---
+++ MidiMedium/Dummy/MidiMediumDummy.h
@@ -0,0 +1,54 @@
+#ifndef MIDI_MEDIUM_DUMMY_H
+#define MIDI_MEDIUM_DUMMY_H
+
+#include "IMidiInMedium.h"
+#include "IMidiOutMedium.h"
+#include <memory>
+#include <string>
+
+namespace midi
+{
+
+class MidiInMediumDummy : public IMidiInMedium
+{
+public:
+    MidiInMediumDummy(const std::string& usbDeviceName, IMidiMedium::Type type);
+
+    IMidiMedium::Type getType() const override;
+    std::string getPortName() const override;
+    std::string getDeviceName() const override;
+    void registerCallback(Callback cb) override;
+    void update() override;
+private:
+    std::string       m_usbDeviceName;
+    IMidiMedium::Type m_type;
+};
+
+class MidiOutMediumDummy : public IMidiOutMedium
+{
+public:
+    MidiOutMediumDummy(const std::string& usbDeviceName, IMidiMedium::Type type);
+
+    IMidiMedium::Type getType() const override;
+    std::string getPortName() const override;
+    std::string getDeviceName() const override;
+    bool send(const std::vector<uint8_t>& message) override;
+private:
+    std::string       m_usbDeviceName;
+    IMidiMedium::Type m_type;
+};
+
+class MidiMediumDummy
+{
+public:
+    MidiMediumDummy(const std::string& usbDeviceName, IMidiMedium::Type type);
+    std::unique_ptr<MidiInMediumDummy> hijackInMedium();
+    std::unique_ptr<MidiOutMediumDummy> hijackOutMedium();
+private:
+    std::unique_ptr<MidiInMediumDummy>  m_pMidiInMediumDummy;
+    std::unique_ptr<MidiOutMediumDummy> m_pMidiOutMediumDummy;
+};
+
+} // namespace midi
+
+#endif
