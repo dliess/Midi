20260cbd03d539905d88717b78f71789573ea40e
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2019-12-19 18:18:24 +0100

    added example

--- MidiMedium/Usb/Ports/Test/main.cpp
+++ MidiMedium/Usb/Ports/Test/main.cpp
@@ -1,48 +1,24 @@
-
-#include "UsbMidiInputPortListProvider.h"
-#include "UsbMidiOutputPortListProvider.h"
 #include "UsbMidiPortNotifier.h"
 #include <iostream>
 #include <unistd.h> // sleep()
 
-using namespace midi;
-
 int main()
 {
-    InputPortListProvider  inputPortListProvider;
-    OutputPortListProvider outputPortListProvider;
-    PortNotifier<InputPortListProvider> inputPortListChangeHandler(inputPortListProvider);
-    PortNotifier<OutputPortListProvider> outputPortListChangeHandler(outputPortListProvider);
+   midi::PortNotifiers::instance().init();
+   midi::PortNotifiers::instance().inputs.registerNewPortCb([](rtmidiadapt::PortIndex portIndex,
+                                                               const rtmidiadapt::DeviceOnUsbPort& devOnPort){
+      std::cout << "Inputs: " << std::endl << " - PortIndex: " << portIndex << "devOnPort" << devOnPort << std::endl;
+   }, { {}, {}, false } );
 
-    if(!inputPortListProvider.init())
-    {
-        std::cerr << "could not initialize inputPortListProvider" << std::endl;
-        return -1;
-    }
-    if(!outputPortListProvider.init())
-    {
-        std::cerr << "could not initialize outputPortListProvider" << std::endl;
-        return -1;
-    }
+   midi::PortNotifiers::instance().outputs.registerNewPortCb([](rtmidiadapt::PortIndex portIndex,
+                                                               const rtmidiadapt::DeviceOnUsbPort& devOnPort){
+      std::cout << "Outputs: " << std::endl << " - PortIndex: " << portIndex << "devOnPort" << devOnPort << std::endl;
+   }, { {}, {}, false } );
 
-    inputPortListChangeHandler.registerNewPortCb([](rtmidiadapt::PortIndex i, const rtmidiadapt::DeviceOnUsbPort& devOnUsbPort){
-        std::cout << "New input " << i << " : " << devOnPort << std::endl;
-    });
-    inputPortListChangeHandler.registerRemovedPortCb([](const rtmidiadapt::DeviceOnUsbPort& devOnUsbPort){
-        std::cout << "Removed input " << devOnPort << std::endl;
-    });
-    outputPortListChangeHandler.registerNewPortCb([](rtmidiadapt::PortIndex i, const rtmidiadapt::DeviceOnUsbPort& devOnUsbPort){
-        std::cout << "New output " << i << " : " << devOnPort << std::endl;
-    });
-    outputPortListChangeHandler.registerRemovedPortCb([](const rtmidiadapt::DeviceOnUsbPort& devOnUsbPort){
-        std::cout << "Removed output " << devOnPort << std::endl;
-    });
-
-    while(true)
-    {
-        inputPortListChangeHandler.update();
-        outputPortListChangeHandler.update();
-        sleep(1);
-    }
-    return 0;
+   while(true)
+   {
+      midi::PortNotifiers::instance().update();
+      sleep(1);
+   }
+   return 0;
 }
