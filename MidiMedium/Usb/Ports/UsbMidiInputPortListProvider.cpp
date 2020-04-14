c7049259cb1a2c949bac674da8e9b5159679e10a
Zicsi-Liess, Daniel <Daniel.Zicsi-Liess@kuka.com>
2020-04-14 22:21:33 +0200

    some refactoring

--- MidiMedium/Usb/Ports/UsbMidiInputPortListProvider.cpp
+++ MidiMedium/Usb/Ports/UsbMidiInputPortListProvider.cpp
@@ -1,45 +1,45 @@
 #include "UsbMidiInputPortListProvider.h"
+
 #include <rtmidi/RtMidi.h>
+
+#include <cassert>
 #include <iostream>
 
 using namespace midi;
-  
-InputPortListProvider::InputPortListProvider() :
-    m_pMidiIn(nullptr)
-{
-}
 
-InputPortListProvider::~InputPortListProvider()
-{
-    delete m_pMidiIn;
-}
+InputPortListProvider::InputPortListProvider() : m_pMidiIn(nullptr) {}
+
+InputPortListProvider::~InputPortListProvider() { delete m_pMidiIn; }
 
 bool InputPortListProvider::init()
 {
-    try
-    {
-        if(!m_pMidiIn)
-            m_pMidiIn = new RtMidiIn(__RTMIDI_API__);
-    }
-    catch ( RtMidiError &error )
-    {
-        error.printMessage();
-        return false;
-    }
-    return true;
+   try
+   {
+      if (!m_pMidiIn)
+         m_pMidiIn = new RtMidiIn(__RTMIDI_API__);
+   }
+   catch (RtMidiError &error)
+   {
+      error.printMessage();
+      return false;
+   }
+   return true;
 }
 
-bool InputPortListProvider::getPorts(PortInfoSet& rPortSet)
+bool InputPortListProvider::getPorts(PortInfoSet &rPortSet)
 {
-    for(unsigned int i=0; i < m_pMidiIn->getPortCount(); ++i) 
-    {
-        try {
-            rPortSet.insert(PortInfo(i, m_pMidiIn->getPortName(i)));
-        }
-        catch ( RtMidiError &error ) {
-            error.printMessage();
-            return false;
-        }
-    }
-    return true;
+   assert(m_pMidiIn != nullptr);
+   for (unsigned int i = 0; i < m_pMidiIn->getPortCount(); ++i)
+   {
+      try
+      {
+         rPortSet.insert(PortInfo(i, m_pMidiIn->getPortName(i)));
+      }
+      catch (RtMidiError &error)
+      {
+         error.printMessage();
+         return false;
+      }
+   }
+   return true;
 }
