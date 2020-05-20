60e32ad8317c8a552543fdcb413259c5374f8ae9
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2020-05-20 06:35:02 +0200

    extended the port-test

--- MidiMedium/Usb/Ports/Test/main.cpp
+++ MidiMedium/Usb/Ports/Test/main.cpp
@@ -7,12 +7,20 @@ int main()
    midi::PortNotifiers::instance().init();
    midi::PortNotifiers::instance().inputs.registerNewPortCb([](rtmidiadapt::PortIndex portIndex,
                                                                const rtmidiadapt::DeviceOnUsbPort& devOnPort){
-      std::cout << "Inputs: PortIndex: " << portIndex << " devOnPort: " << devOnPort << std::endl;
+      std::cout << "ADDED Inputs: PortIndex: " << portIndex << " devOnPort: " << devOnPort << std::endl;
    }, { {}, {}, false } );
 
    midi::PortNotifiers::instance().outputs.registerNewPortCb([](rtmidiadapt::PortIndex portIndex,
                                                                const rtmidiadapt::DeviceOnUsbPort& devOnPort){
-      std::cout << "Outputs: PortIndex: " << portIndex << " devOnPort: " << devOnPort << std::endl;
+      std::cout << "ADDED Outputs: PortIndex: " << portIndex << " devOnPort: " << devOnPort << std::endl;
+   }, { {}, {}, false } );
+
+   midi::PortNotifiers::instance().inputs.registerRemovedPortCb([](const rtmidiadapt::DeviceOnUsbPort& devOnPort){
+      std::cout << "REMOVED Inputs: devOnPort: " << devOnPort << std::endl;
+   }, { {}, {}, false } );
+
+   midi::PortNotifiers::instance().outputs.registerRemovedPortCb([](const rtmidiadapt::DeviceOnUsbPort& devOnPort){
+      std::cout << "REMOVED Outputs: devOnPort: " << devOnPort << std::endl;
    }, { {}, {}, false } );
 
    while(true)
