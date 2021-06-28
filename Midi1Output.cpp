2b5f4350add69fdcedbf355f9022a6ca8ba60889
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2021-06-28 19:51:26 +0000

    transportLogic

--- Midi1Output.cpp
+++ Midi1Output.cpp
@@ -1,112 +1,176 @@
 #include "Midi1Output.h"
-#include "MidiMessage.h"
-#include "IMidiOutMedium.h"
+
 #include <iostream>
+
+#include "IMidiOutMedium.h"
+#include "MidiMessage.h"
 
 using namespace midi;
 
 Midi1Output::Midi1Output(std::unique_ptr<IMidiOutMedium> pMedium) :
     m_pMedium(std::move(pMedium)),
-    m_xrpnOutputHandler([this](const Message<ControlChange>& msg){ send(msg); }),
-    m_CCHighResOutputHandler([this](const Message<ControlChange>& msg){ send(msg); })
+    m_xrpnOutputHandler(
+        [this](const Message<ControlChange> &msg) { send(msg); }),
+    m_CCHighResOutputHandler(
+        [this](const Message<ControlChange> &msg) { send(msg); })
 {
 }
 
-IMidiOutMedium& Midi1Output::medium()
-{
-    return *m_pMedium;
-}
+IMidiOutMedium &Midi1Output::medium() { return *m_pMedium; }
 
-const IMidiOutMedium& Midi1Output::medium() const
-{
-    return *m_pMedium;
-}
+const IMidiOutMedium &Midi1Output::medium() const { return *m_pMedium; }
 
 bool Midi1Output::noteOn(uint8_t channel, uint8_t note, uint8_t velocity)
 {
-    return send( Message<NoteOn>(channel, note, velocity) );
+   return send(Message<NoteOn>(channel, note, velocity));
 }
 
 bool Midi1Output::noteOff(uint8_t channel, uint8_t note, uint8_t velocity)
 {
-    return send( Message<NoteOff>(channel, note, velocity) );
+   return send(Message<NoteOff>(channel, note, velocity));
 }
 
 bool Midi1Output::pitchBend(uint8_t channel, int16_t value)
 {
-    return send( Message<PitchBend>(channel, value) );
+   return send(Message<PitchBend>(channel, value));
 }
 
 bool Midi1Output::afterTouchPoly(uint8_t channel, uint8_t note, int16_t value)
 {
-    return send(Message<AfterTouchPoly>(channel, note, value));
+   return send(Message<AfterTouchPoly>(channel, note, value));
 }
 
 bool Midi1Output::afterTouch(uint8_t channel, int16_t value)
 {
-    return send(Message<AfterTouchChannel>(channel, value));
+   return send(Message<AfterTouchChannel>(channel, value));
 }
 
 bool Midi1Output::controlParameter(uint8_t channel, uint8_t ccId, uint8_t value)
 {
-    return send( Message<ControlChange>(channel, ccId, value) );
+   return send(Message<ControlChange>(channel, ccId, value));
 }
 
-bool Midi1Output::sysEx(const std::vector<uint8_t>& msg)
+bool Midi1Output::start()
 {
-    return m_pMedium->send(msg);
+   if (m_transportMasked)
+   {
+      return true;
+   }
+   bool ret{false};
+   ret       = send(Message<Start>());
+   m_started = true;
+   for (auto cb : m_startedChangedCbs) cb(m_started);
+   return ret;
 }
 
-bool Midi1Output::send(const MidiMessage& msg)
+bool Midi1Output::stop()
 {
-    mpark::visit(overload{
-        [this](const Message<NoteOn>& msg) { send(msg); },
-        [this](const Message<NoteOff>& msg) { send(msg); },
-        [this](const Message<AfterTouchPoly>& msg) { send(msg); },
-        [this](const Message<ControlChange>& msg) { send(msg); },
-        [this](const Message<ProgramChange>& msg) { send(msg); },
-        [this](const Message<AfterTouchChannel>& msg) { send(msg); },
-        [this](const Message<PitchBend>& msg) { send(msg); },
-        [this](const Message<TimeCodeQuarterFrame>& msg) { send(msg); },
-        [this](const Message<SongPosition>& msg) { send(msg); },
-        [this](const Message<SongSelect>& msg) { send(msg); },
-        [this](const Message<TuneRequest>& msg) { send(msg); },
-        [this](const Message<Clock>& msg) { send(msg); },
-        [this](const Message<Start>& msg) { send(msg); },
-        [this](const Message<Continue>& msg) { send(msg); },
-        [this](const Message<Stop>& msg) { send(msg); },
-        [this](const Message<ActiveSensing>& msg) { send(msg); },
-        [this](const Message<SystemReset>& msg) { send(msg); },
-        [this](const Message<RPN>& msg) { m_xrpnOutputHandler.send(msg); },
-        [this](const Message<NRPN>& msg) { m_xrpnOutputHandler.send(msg); },
-        [this](const Message<ControlChangeHighRes>& msg) { m_CCHighResOutputHandler.send(msg); },
-        [](auto&& other) { std::cerr << "INTERNAL ERROR, unknown midi message layout" << std::endl; }
-    }, msg);
-    return true;
+   if (m_transportMasked)
+   {
+      return true;
+   }
+   bool ret{false};
+   ret       = send(Message<Stop>());
+   m_started = false;
+   for (auto cb : m_startedChangedCbs) cb(m_started);
+   return ret;
 }
 
-bool Midi1Output::send(const MsgLayout<1>& midiMessage)
+bool Midi1Output::toggleStartStop()
 {
-    std::vector<uint8_t> msg(1);
-    msg[0] = midiMessage.command();
-    return m_pMedium->send(msg);
+   if (!m_started)
+   {
+      return start();
+   }
+   else
+   {
+      return stop();
+   }
 }
 
-bool Midi1Output::send(const MsgLayout<2>& midiMessage)
+void Midi1Output::setTransportMasked(bool masked)
 {
-    std::vector<uint8_t> msg(2);
-    msg[0] = midiMessage.command();
-    msg[1] = midiMessage.data1();
-    return m_pMedium->send(msg);
+   if (m_transportMasked == masked)
+   {
+      return;
+   }
+   if (masked)
+   {
+       if(m_started) stop();
+   }
+   m_transportMasked = masked;
+    for(auto cb : m_transportMaskChangedCbs) cb(m_transportMasked);
 }
 
-bool Midi1Output::send(const MsgLayout<3>& midiMessage)
+void Midi1Output::registerStartedChangedCb(StartedChangedCb cb)
 {
-    std::vector<uint8_t> msg(3);
-    msg[0] = midiMessage.command();
-    msg[1] = midiMessage.data1();
-    msg[2] = midiMessage.data2();
-    return m_pMedium->send(msg);
+   m_startedChangedCbs.push_back(cb);
 }
 
+void Midi1Output::registerTransportMaskChangedCb(TransportMaskChangedCb cb)
+{
+    m_transportMaskChangedCbs.push_back(cb);
+}
 
+bool Midi1Output::sysEx(const std::vector<uint8_t> &msg)
+{
+   return m_pMedium->send(msg);
+}
+
+bool Midi1Output::send(const MidiMessage &msg)
+{
+   mpark::visit(
+       overload{
+           [this](const Message<NoteOn> &msg) { send(msg); },
+           [this](const Message<NoteOff> &msg) { send(msg); },
+           [this](const Message<AfterTouchPoly> &msg) { send(msg); },
+           [this](const Message<ControlChange> &msg) { send(msg); },
+           [this](const Message<ProgramChange> &msg) { send(msg); },
+           [this](const Message<AfterTouchChannel> &msg) { send(msg); },
+           [this](const Message<PitchBend> &msg) { send(msg); },
+           [this](const Message<TimeCodeQuarterFrame> &msg) { send(msg); },
+           [this](const Message<SongPosition> &msg) { send(msg); },
+           [this](const Message<SongSelect> &msg) { send(msg); },
+           [this](const Message<TuneRequest> &msg) { send(msg); },
+           [this](const Message<Clock> &msg) { send(msg); },
+           [this](const Message<Start> &msg) { send(msg); },
+           [this](const Message<Continue> &msg) { send(msg); },
+           [this](const Message<Stop> &msg) { send(msg); },
+           [this](const Message<ActiveSensing> &msg) { send(msg); },
+           [this](const Message<SystemReset> &msg) { send(msg); },
+           [this](const Message<RPN> &msg) { m_xrpnOutputHandler.send(msg); },
+           [this](const Message<NRPN> &msg) { m_xrpnOutputHandler.send(msg); },
+           [this](const Message<ControlChangeHighRes> &msg) {
+              m_CCHighResOutputHandler.send(msg);
+           },
+           [](auto &&other) {
+              std::cerr << "INTERNAL ERROR, unknown midi message layout"
+                        << std::endl;
+           }},
+       msg);
+   return true;
+}
+
+bool Midi1Output::send(const MsgLayout<1> &midiMessage)
+{
+   std::vector<uint8_t> msg(1);
+   msg[0] = midiMessage.command();
+   return m_pMedium->send(msg);
+}
+
+bool Midi1Output::send(const MsgLayout<2> &midiMessage)
+{
+   std::vector<uint8_t> msg(2);
+   msg[0] = midiMessage.command();
+   msg[1] = midiMessage.data1();
+   return m_pMedium->send(msg);
+}
+
+bool Midi1Output::send(const MsgLayout<3> &midiMessage)
+{
+   std::vector<uint8_t> msg(3);
+   msg[0] = midiMessage.command();
+   msg[1] = midiMessage.data1();
+   msg[2] = midiMessage.data2();
+   return m_pMedium->send(msg);
+}
--- Midi1Output.h
+++ Midi1Output.h
@@ -9,7 +9,7 @@
 #include <memory>
 #include <vector>
 #include <cstdint>
-
+#include <functional>
 namespace midi
 {
 
@@ -25,6 +25,14 @@ public:
     bool afterTouchPoly(uint8_t channel, uint8_t note, int16_t value);
     bool afterTouch(uint8_t channel, int16_t value);
     bool controlParameter(uint8_t channel, uint8_t ccId, uint8_t value);
+    bool start();
+    bool stop();
+    bool toggleStartStop();
+    void setTransportMasked(bool masked);
+    using StartedChangedCb = std::function<void(bool)>;
+    void registerStartedChangedCb(StartedChangedCb cb);
+    using TransportMaskChangedCb = std::function<void(bool)>;
+    void registerTransportMaskChangedCb(TransportMaskChangedCb cb);
     bool sysEx(const std::vector<uint8_t>& msg);
     bool send(const MidiMessage& msg);
     bool send(const MsgLayout<1>& midiMessage);
@@ -34,6 +42,10 @@ private:
     std::unique_ptr<IMidiOutMedium> m_pMedium;
     xrpn::XRpnOutputHandler         m_xrpnOutputHandler;
     CCHighResOutputHandler          m_CCHighResOutputHandler;
+    bool m_started{false};
+    bool m_transportMasked{false};
+    std::vector<StartedChangedCb> m_startedChangedCbs;
+    std::vector<TransportMaskChangedCb> m_transportMaskChangedCbs;
 };
 
 } // namespace midi
