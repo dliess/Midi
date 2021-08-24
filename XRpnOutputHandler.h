3221678cf3aee7d953bee4e4b7c43d2137483fcf
Zicsi-Liess <liessdaniel415@gmail.com>
2021-08-24 12:38:53 +0000

    added runningStatus support parameter to XRPN send

--- XRpnOutputHandler.h
+++ XRpnOutputHandler.h
@@ -15,19 +15,19 @@ public:
     XRpnOutputHandler(CCConsumerCb cb) noexcept;
 
     template<typename XRPN_TYPE>
-    void send(const XRPN_TYPE& msg) noexcept;
+    void send(const XRPN_TYPE& msg, bool withRunningStatus = false) noexcept;
 private:
     std::array<MidiMessage, NUM_CHANNELS> m_activeXRpn;
     CCConsumerCb                          m_ccConsumerCb;
     void sendClearRpn(int channelNr) noexcept;
     template<typename XRPN_TYPE>
-    void sendXRpn(XRPN_TYPE& cachedXrpn, const XRPN_TYPE& msg) noexcept;
+    void sendXRpn(XRPN_TYPE& cachedXrpn, const XRPN_TYPE& msg, bool withRunningStatus) noexcept;
 };
 
 } // namespace midi::xrpn
 
 template<typename XRPN_TYPE>
-void midi::xrpn::XRpnOutputHandler::send(const XRPN_TYPE& msg) noexcept
+void midi::xrpn::XRpnOutputHandler::send(const XRPN_TYPE& msg, bool withRunningStatus) noexcept
 {
     const auto channelIdx = msg.channelNr - 1;
     auto& active = m_activeXRpn[channelIdx];
@@ -35,15 +35,15 @@ void midi::xrpn::XRpnOutputHandler::send(const XRPN_TYPE& msg) noexcept
         active.template emplace<XRPN_TYPE>(msg.channelNr);
     }
     try{
-        sendXRpn(mpark::get<XRPN_TYPE>(active), msg);
+        sendXRpn(mpark::get<XRPN_TYPE>(active), msg, withRunningStatus);
     }catch(...) {/* should not happen */}
 }
 
 template<typename XRPN_TYPE>
-void midi::xrpn::XRpnOutputHandler::sendXRpn(XRPN_TYPE& cachedXrpn, const XRPN_TYPE& msg) noexcept
+void midi::xrpn::XRpnOutputHandler::sendXRpn(XRPN_TYPE& cachedXrpn, const XRPN_TYPE& msg, bool withRunningStatus) noexcept
 {
-    if(cachedXrpn.idMsb != msg.idMsb || cachedXrpn.idLsb != msg.idLsb){
-        sendClearRpn(msg.channelNr);
+    if(!withRunningStatus || cachedXrpn.idMsb != msg.idMsb || cachedXrpn.idLsb != msg.idLsb){
+        // sendClearRpn(msg.channelNr);
         cachedXrpn.idMsb = msg.idMsb;
         cachedXrpn.idLsb = msg.idLsb;
         cachedXrpn.valueMsb = msg.valueMsb;
