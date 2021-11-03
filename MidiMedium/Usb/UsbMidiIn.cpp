7febefcae359e175672fe244d77dfd665639d0c0
Zicsi-Liess <liessdaniel415@gmail.com>
2021-11-03 13:03:00 +0000

    ignore clock and activeSense

--- MidiMedium/Usb/UsbMidiIn.cpp
+++ MidiMedium/Usb/UsbMidiIn.cpp
@@ -28,7 +28,7 @@ bool UsbMidiIn::openPort(int portNmbr) noexcept
         m_pRtMidiIn->openPort(portNmbr);
         m_pRtMidiIn->setCallback(helperCb, this);
         m_pRtMidiIn->setErrorCallback(errorCb, this);
-        m_pRtMidiIn->ignoreTypes( /*sysex*/false, /*timimng*/false, /*sense*/false );
+        m_pRtMidiIn->ignoreTypes( /*sysex*/false, /*timimng*/true, /*sense*/true );
         m_usbPortName = rtmidiadapt::DeviceOnUsbPort( m_pRtMidiIn->getPortName(portNmbr) ).getUsbPortName();
         m_deviceName = rtmidiadapt::DeviceOnUsbPort( m_pRtMidiIn->getPortName(portNmbr) ).getDeviceName();
     }
